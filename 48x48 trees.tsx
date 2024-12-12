3e6f752c64352555335ab77cea66d11ff779459f
jess <jessforster@yahoo.com>
2024-12-12 15:31:46 -0300

    aaaa

--- 48x48 trees.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="48x48 trees" tilewidth="48" tileheight="48" tilecount="4" columns="4">
- <image source="../../../Downloads/48x48 trees.png" width="192" height="48"/>
-</tileset>
